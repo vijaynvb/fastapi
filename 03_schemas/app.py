86ae4d03c4a60dcc37879b37d74fd0bb3373b590
vijaynvb <vijay.nvb@gmail.com>
2026-07-21 12:56:46 +0530

    Refactor employee schemas and endpoints to use UUIDs for unique identification

--- 03_schemas/app.py
+++ 03_schemas/app.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
-from schemas import EmployeeSchema
-import uvicorn 
+from schemas import ResponseEmployeeSchema, RequestEmployeeSchema
+import uvicorn
+import uuid
 
-employees: list[EmployeeSchema] = [
-    EmployeeSchema(id=1, name="John Doe", position="Software Engineer", salary=80000.0, is_active=True),
-    EmployeeSchema(id=2, name="Jane Smith", position="Data Scientist", salary=90000.0, is_active=True),
-    EmployeeSchema(id=3, name="Mike Johnson", position="Product Manager", salary=95000.0, is_active=False),
-    EmployeeSchema(id=4, name="Emily Davis", position="UX Designer", salary=70000.0, is_active=True),
+employees: list[ResponseEmployeeSchema] = [
+    ResponseEmployeeSchema(id=uuid.uuid4(), name="John Doe", position="Software Engineer", salary=80000.0, is_active=True),
+    ResponseEmployeeSchema(id=uuid.uuid4(), name="Jane Smith", position="Data Scientist", salary=90000.0, is_active=True),
+    ResponseEmployeeSchema(id=uuid.uuid4(), name="Mike Johnson", position="Product Manager", salary=95000.0, is_active=False),
+    ResponseEmployeeSchema(id=uuid.uuid4(), name="Emily Davis", position="UX Designer", salary=70000.0, is_active=True),
 ]
 
 app = FastAPI()
 
-@app.get("/employees", response_model=list[EmployeeSchema])
+@app.get("/employees", response_model=list[ResponseEmployeeSchema], status_code=200)
 def read_root():
     return employees
 
-@app.post("/employees", response_model=EmployeeSchema, status_code=201)
-def create_employee(employee: EmployeeSchema):
-    employees.append(employee)
-    return employee
+@app.post("/employees", response_model=ResponseEmployeeSchema, status_code=201)
+def create_employee(employee: RequestEmployeeSchema):
+    #uuid - unique identifier
+    new_employee = ResponseEmployeeSchema(id=uuid.uuid4(), name=employee.name, position=employee.position, salary=employee.salary, is_active=employee.is_active)
+    employees.append(new_employee)
+    return new_employee
--- 03_schemas/schemas.py
+++ 03_schemas/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+import uuid
 
 class UserSchema(BaseModel):
     id: int
@@ -6,12 +7,19 @@ class UserSchema(BaseModel):
     email: str
     is_active: bool
 
-class EmployeeSchema(BaseModel):
-    id: int
+class ResponseEmployeeSchema(BaseModel):
+    id: uuid.UUID
     name: str
     position: str
     salary: float
     is_active: bool
+
+class RequestEmployeeSchema(BaseModel):
+    name: str
+    position: str
+    salary: float
+    is_active: bool
+
 
 class DepartmentSchema(BaseModel):
     id: int
