5a401e174bacc866bbf061128a61554018f3ea72
vijaynvb <vijay.nvb@gmail.com>
2026-07-21 10:56:04 +0530

    Add FastAPI application and endpoints for employee routes

---
+++ 10_exercise/01-task.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, FastAPI
+import uvicorn 
+
+app = FastAPI(
+    title="My API",
+    description="This is a sample API",
+    version="1.0.0"
+)
+
+global_router_v1 = APIRouter(prefix="/api/v1")
+global_router_v2 = APIRouter(prefix="/api/v2")
+employee_router_v1 = APIRouter(prefix="/employees")
+employee_router_v2 = APIRouter(prefix="/employees")
+
+# api/v1/employees 
+# api/v1/departments
+
+@employee_router_v1.get("/")
+def read_root_v1():
+    return {"name": "vijay", "age": 30}
+
+@employee_router_v2.get("/")
+def read_root_v2():
+    return {"name": "vijay", "age": 30}
+
+global_router_v1.include_router(employee_router_v1)
+global_router_v2.include_router(employee_router_v2)
+app.include_router(global_router_v1)
+app.include_router(global_router_v2)
---
+++ 10_exercise/task.md
@@ -0,0 +1,12 @@
+
+# Tasks
+
+### Task 1
+- Create a new FastAPI application with the following specifications:
+  - Title: "My API"
+  - Description: "This is a sample API"
+  - Version: "1.0.0"
+
+create endpoints for the following routes using APIRouter:
+  - GET /api/v1/employees
+  - GET /api/v2/employees
